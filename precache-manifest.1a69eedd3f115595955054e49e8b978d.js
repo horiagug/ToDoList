9880eacc6c034d38351cf2964233b9a4fa0b2291
horiagug <hg_hel@protonmail.com>
2020-09-05 21:22:54 +0300

    Updates

--- precache-manifest.1a69eedd3f115595955054e49e8b978d.js
+++ precache-manifest.1a69eedd3f115595955054e49e8b978d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "782f04579863f1451fb019f21ac7f543",
+    "revision": "9072d58f8d8e81b9bd5527d22b3e882d",
     "url": "/ToDoList/index.html"
   },
   {
-    "revision": "dffb812cde8aa2fd0cd7",
+    "revision": "623c1c64c0633864b38a",
     "url": "/ToDoList/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ToDoList/static/js/2.1f1575e7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dffb812cde8aa2fd0cd7",
-    "url": "/ToDoList/static/js/main.5afff5b1.chunk.js"
+    "revision": "623c1c64c0633864b38a",
+    "url": "/ToDoList/static/js/main.c6bf7d3f.chunk.js"
   },
   {
     "revision": "95ab476d57fadd6c0226",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ToDoList/precache-manifest.449aa934f920f19e8b93197bd7fca571.js"
+  "/ToDoList/precache-manifest.1a69eedd3f115595955054e49e8b978d.js"
 );
 
 self.addEventListener('message', (event) => {
