56048f1fb0c705eab638ca012eaeb41b659282c4
horiagug <hg_hel@protonmail.com>
2020-09-05 21:08:30 +0300

    Updates

---
+++ precache-manifest.49b35e1499f13728ff2683da262a6e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e76624e7a609d6583902e51e85ca093",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "f44b8a71ef5fb43894ec",
+    "url": "/ToDoList/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "38e7ba5537651a212432",
+    "url": "/ToDoList/static/js/2.82cfbc36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDoList/static/js/2.82cfbc36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44b8a71ef5fb43894ec",
+    "url": "/ToDoList/static/js/main.ef8c8079.chunk.js"
+  },
+  {
+    "revision": "95ab476d57fadd6c0226",
+    "url": "/ToDoList/static/js/runtime-main.2b209adc.js"
+  }
+]);
